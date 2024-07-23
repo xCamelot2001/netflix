3205ca3ee70a315749a22f522b6b5022ef7aa924
Hossein Masjedi <120756379+xCamelot2001@users.noreply.github.com>
2024-07-24 00:25:39 +0100

    1st sesh netflix starting

---
+++ src/global-styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    background-color: black;
+    color: #333333;
+    font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
